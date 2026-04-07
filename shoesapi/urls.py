4a8b9d0ad8241c728630ce335857102a11dcbc6c
ShivaSup <shivashankarr9035@gmail.com>
2026-04-07 13:34:52 +0530

    Shoes to shoes

--- shoesapi/urls.py
+++ shoesapi/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Shoes/',ShoeView.as_view()),
+    path('shoes/',ShoeView.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
